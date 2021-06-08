97f8ba828699ec406079a241b8b0eca28b6b2a5f
msaaddev <mrsaadirfan@gmail.com>
2021-06-08 23:56:51 +0500

    👌 IMPROVE: name

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-const asyncExec = require('./index');
+const execAsync = require('./index');
 const handleError = require('node-cli-handle-error');
 
 (async () => {
 	try {
 		// executes a shell command asynchronously
-		await asyncExec({ cmd: `touch example.md` });
+		await execAsync({ cmd: `touch abc.md` });
 
 		// changes directory and then run the shell command inside that directory
-		await asyncExec({
+		await execAsync({
 			path: `/Users/saadirfan/GitHub`,
 			cmd: `touch example.md`
 		});
